7d7dc6dcd01ea5111bd04702067761eea9076793
lsp8 <lahude.paulo@gmail.com>
2023-02-09 11:32:51 -0300

    models, helpers

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
 import React, {useRef, useState} from 'react';
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {
+  Modal,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ActivityIndicator,
+} from 'react-native';
 
 import {Camera} from 'expo-camera';
 import {
@@ -9,12 +16,13 @@ import {
 } from './src/helpers/tensor-helper';
 import {cropPicture} from './src/helpers/image-helper';
 
+const RESULT_MAPPING = ['Triangle', 'Circle', 'Square'];
+
 export default function App() {
-  const RESULT_MAPPING = ['Filled', 'Empty'];
   const cameraRef = useRef();
-  const [isProcessing, setIsProcessing] = useState(false);
-  const [permission, setPermission] = Camera.useCameraPermissions();
   const [camera, setCamera] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [presentedShape, setPresentedShape] = useState('');
 
   const handleCamera = async () => {
     const {status} = await Camera.requestCameraPermissionsAsync();
@@ -25,8 +33,48 @@ export default function App() {
     }
   };
 
+  const handleImageCapture = async () => {
+    setIsProcessing(true);
+    try {
+      const imageData = await cameraRef.current.takePictureAsync({
+        base64: true,
+      });
+      processImagePrediction(imageData);
+    } catch {}
+  };
+
+  const processImagePrediction = async base64Image => {
+    const croppedData = await cropPicture(base64Image, 300);
+    const model = await getModel();
+    const tensor = await convertBase64ToTensor(croppedData.base64);
+    const prediction = await startPrediction(model, tensor);
+    const highestPrediction = prediction.indexOf(
+      Math.max.apply(null, prediction),
+    );
+    setPresentedShape(RESULT_MAPPING[highestPrediction]);
+  };
+
+  const handleCloseModal = () => {
+    setIsProcessing(false);
+    setPresentedShape('');
+  };
+
   return (
     <View style={styles.container}>
+      <Modal visible={isProcessing} transparent={true} animationType="slide">
+        <View style={styles.modal}>
+          <Text style={styles.modalText}>
+            A forma atual é: {presentedShape}
+          </Text>
+          {presentedShape === '' && <ActivityIndicator size="large" />}
+          <TouchableOpacity
+            onPress={() => {
+              handleCloseModal();
+            }}>
+            <Text style={styles.modalText}>Fechar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
       {camera && (
         <Camera
           ref={cameraRef}
@@ -35,12 +83,29 @@ export default function App() {
           autoFocus={true}
         />
       )}
-
-      <View>
-        <TouchableOpacity style={styles.camButton} onPress={handleCamera}>
-          <Text style={styles.camButtonText}>Câmera</Text>
-        </TouchableOpacity>
-      </View>
+      {camera && (
+        <View>
+          <TouchableOpacity
+            style={styles.camButton}
+            onPress={() => handleImageCapture()}>
+            <Text style={styles.camButtonText}>Capturar imagem</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.camButton}
+            onPress={() => setCamera(false)}>
+            <Text style={styles.camButtonText}>Fechar Câmera</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {!camera && (
+        <View>
+          <TouchableOpacity
+            style={styles.camButton}
+            onPress={() => handleCamera()}>
+            <Text style={styles.camButtonText}>Abrir camera</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
@@ -73,5 +138,18 @@ const styles = StyleSheet.create({
     marginTop: '70%',
     zIndex: 1,
     alignSelf: 'center',
+  },
+  modal: {
+    width: '90%',
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'gray',
+    alignSelf: 'center',
+    marginTop: '20%',
+  },
+  modalText: {
+    color: 'white',
+    fontSize: 20,
   },
 });
